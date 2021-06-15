8a39e4398593cadde5ea1ea1709be1a61abb1e0c
RAGE UDAY KIRAN <52146396+udayRage@users.noreply.github.com>
2021-06-15 22:52:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'pami',
-    version = '0.0.9.1.8',
+    version = '0.9.2',
     author = 'Rage Uday Kiran',
     author_email = 'uday.rage@gmail.com',
     description = 'Pattern mining',
-    long_description = long_description,
+    long_description = 'The main purpose of this software is to two folds. First, promote research in the field of pattern mining. Second, facilitate industrial application of pattern mining.',
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/udayRage/PAMI.git',
     packages = setuptools.find_packages(),
     classifiers = [
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNUv3',
         'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
     ],
