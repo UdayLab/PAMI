8cd8aa30f5b818e7967f35d0ef09ed1156ee0a13
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-06-28 16:42:02 +0530

    Update cpfp.py

--- PAMI/periodicFrequentPattern/closed/cpfp.py
+++ PAMI/periodicFrequentPattern/closed/cpfp.py
@@ -65,7 +65,7 @@ class CPFPMiner(periodicFrequentPatterns):
         
         Sample run of the imported code:
         --------------
-        import cpfp as alg
+        from PAMI.periodicFrequentPattern.basic import cpfp as alg
 
         obj = alg.CPFPMiner("../basic/sampleTDB.txt", "2", "6")
 
