65ab769f2d2ef17111271b9fa5d671f3c2e812a9
So Nakamura <s1270204@u-aizu.ac.jp>
2021-07-15 18:14:18 +0900

    0.9.6.5.1

---
+++ PAMI/dataProcessing/dense2DB.py
@@ -0,0 +1,180 @@
+import pandas as pd
+
+class dense2DB:
+    """
+        This class create Data Base from DataFrame.
+        Attribute:
+        ----------
+        inputDF : pandas.DataFrame
+            It is dense DataFrame
+        condition : str
+            It is condition to judge the value in dataframe
+        thresholdValue : int or float:
+            User defined value.
+        tids : list
+            It is tids list.
+        items : list
+            Store the items list
+        outputFile : str
+            Creation data base output to this outputFile.
+
+        Methods:
+        --------
+        createDB(outputFile)
+            Create transactional data base from dataFrame
+        createTDB(outputFile)
+            Create temporal dataBase from dataFrame
+        getFileName()
+            Return outputFileName.
+        """
+    def __init__(self, inputDF, condition, thresholdValue):
+        self.inputDF = inputDF
+        self.condition = condition
+        self.thresholdValue = thresholdValue
+        self.tids = []
+        self.items = []
+        self.outputFile = ' '
+        self.items = list(self.inputDF.columns.values)[1:]
+        self.inputDF = self.inputDF.set_index('tid')
+        self.tids = list(self.inputDF.index)
+
+
+    def createTransactional(self, outputFile):
+        """
+        Create transactional data base
+        :param outputFile: Write transactional data base into outputFile
+        :type outputFile: str
+        """
+        self.outputFile = outputFile
+        with open(outputFile, 'w') as f:
+            if self.condition == '>':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] > self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{transaction[0]}')
+                        for item in transaction[1:]:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '>=':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] >= self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{transaction[0]}')
+                        for item in transaction[1:]:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '<=':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] <= self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{transaction[0]}')
+                        for item in transaction[1:]:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '<':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] < self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{transaction[0]}')
+                        for item in transaction[1:]:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+            else:
+                print('Condition error')
+
+
+
+    def createTemporal(self, outputFile):
+        """
+        Create temporal data base
+        :param outputFile: Write temporal data base into outputFile
+        :type outputFile: str
+        """
+        self.outputFile = outputFile
+        with open(outputFile, 'w') as f:
+            if self.condition == '>':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] > self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{tid}')
+                        for item in transaction:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{tid}')
+                        f.write(f',{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '>=':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] >= self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{tid}')
+                        for item in transaction:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{tid}')
+                        f.write(f',{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '<=':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] <= self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{tid}')
+                        for item in transaction:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{tid}')
+                        f.write(f',{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            elif self.condition == '<':
+                for tid in self.tids:
+                    transaction = [item for item in self.items if self.inputDF.at[tid, item] < self.thresholdValue]
+                    if len(transaction) > 1:
+                        f.write(f'{tid}')
+                        for item in transaction:
+                            f.write(f',{item}')
+                    elif len(transaction) == 1:
+                        f.write(f'{tid}')
+                        f.write(f',{transaction}')
+                    else:
+                        continue
+                    f.write('\n')
+
+            else:
+                print('Condition error')
+
+    def getFileName(self):
+        """
+        return outputFile name
+        :return: outputFile name
+        """
+        return self.outputFile
+
+
---
+++ PAMI/dataProcessing/sparse2DB.py
@@ -0,0 +1,84 @@
+import pandas as pd
+
+class sparse2DB:
+    """
+    This class create Data Base from DataFrame.
+    Attribute:
+    ----------
+    inputDF : pandas.DataFrame
+        It is sparse DataFrame
+    condition : str
+        It is condition to judge the value in dataframe
+    thresholdValue : int or float:
+        User defined value.
+    DF : pandas.DataFrame
+        It is data frame to create data base.
+    outputFile : str
+        Creation data base output to this outputFile.
+
+    Methods:
+    --------
+    createDB(outputFile)
+        Create transactional data base from dataFrame
+    createTDB(outputFile)
+        Create temporal dataBase from dataFrame
+    getFileName()
+        Return outputFileName.
+    """
+
+    def __init__(self, inputDF, condition, thresholdValue):
+        self.inputDF = inputDF
+        self.condition = condition
+        self.thresholdValue = thresholdValue
+        self.DF = pd.DataFrame
+        self.outputFile = ''
+        if self.condition == '>':
+            self.df = self.inputDF.query(f'value > {self.thresholdValue}')
+        elif self.condition == '>=':
+            self.df = self.inputDF.query(f'value >= {self.thresholdValue}')
+        elif self.condition == '<=':
+            self.df = self.inputDF.query(f'value <= {self.thresholdValue}')
+        elif self.condition == '<':
+            self.df = self.inputDF.query(f'value < {self.thresholdValue}')
+        else:
+            print('Condition error')
+        self.df = self.df.drop(columns='value')
+        self.df = self.df.groupby('tid')['item'].apply(list)
+
+
+    def createDB(self, outputFile):
+        """
+        Create transactional data base
+        :param outputFile: Write transactional data base into outputFile
+        :type outputFile: str
+        """
+        self.outputFile = outputFile
+        with open(self.outputFile, 'w') as f:
+            for line in self.df:
+                f.write(f'{line[0]}')
+                for item in line[1:]:
+                    f.write(f',{item}')
+                f.write('\n')
+
+    def getFileName(self):
+        """
+        return outputFile name
+        :return: outputFile name
+        """
+        return self.outputFile
+
+    def createTDB(self, outputFile):
+        """
+        Create temporal data base
+        :param outputFile: Write temporal data base into outputFile
+        :type outputFile: str
+        """
+        self.outputFile = outputFile
+        with open(self.outputFile, 'w') as f:
+            for tid in self.df.index:
+                f.write(f'{tid}')
+                for item in self.df[tid]:
+                    f.write(f',{item}')
+                f.write('\n')
+
+
