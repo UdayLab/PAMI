67d2d17a8edd42be518963a804dba1e60a61af2e
nakamura0803 <s1270204@u-aizu.ac.jp>
2021-09-02 19:11:58 +0900

    dbStats

---
+++ PAMI/dbStats/transactionalDatabaseStats.py
@@ -0,0 +1,143 @@
+import statistics
+
+class transactionalDatabaseStats:
+    """
+    transactionalDatabaseStats is class to get stats of database.
+
+        Attributes:
+        ----------
+        inputFile : file
+            input file path
+        database : dict
+            store time stamp and its transaction
+        lengthList : list
+            store length of all transaction
+        sep : str
+            separator in file. Default is tab space.
+
+        Methods:
+        -------
+        run()
+            execute readDatabase function
+        readDatabase()
+            read database from input file
+        getDatabaseLength()
+            get the length of database
+        getMinimumTransactionLength()
+            get the minimum transaction length
+        getAverageTransactionLength()
+            get the average transaction length. It is sum of all transaction length divided by database length.
+        getMaximumTransactionLength()
+            get the maximum transaction length
+        getStandardDeviationTransactionLength()
+            get the standard deviation of transaction length
+        getSortedListOfItemFrequencies()
+            get sorted list of item frequencies
+        getSortedListOfTransactionLength()
+            get sorted list of transaction length
+        storeInFile(data, outputFile)
+            store data into outputFile
+    """
+    def __init__(self, inputFile, sep='\t'):
+        """
+        :param inputFile: input file name or path
+        :type inputFile: str
+        """
+        self.inputFile = inputFile
+        self.database = {}
+        self.lengthList = []
+        self.sep = sep
+
+    def run(self):
+        self.readDatabase()
+
+    def readDatabase(self):
+        """
+        read database from input file and store into database and size of each transaction.
+        """
+        numberOfTransaction = 0
+        with open(self.inputFile, 'r') as f:
+            for line in f:
+                numberOfTransaction += 1
+                line = [s for s in line.strip().split(self.sep)]
+                self.database[numberOfTransaction] = self.database.get(numberOfTransaction, set())
+                self.database[numberOfTransaction] = list(set(self.database[numberOfTransaction]) | set(line))
+        self.lengthList = [len(s) for s in self.database.values()]
+
+    def getDatabaseLength(self):
+        """
+        get the Length of database
+        :return: data base length
+        """
+        length = len(self.database)
+        return length
+
+    def getMinimumTransactionLength(self):
+        """
+        get the minimum transaction length
+        :return: minimum transaction length
+        """
+        minLength = min(self.lengthList)
+        return minLength
+
+    def getAverageTransactionLength(self):
+        """
+        get the average transaction length. It is sum of all transaction length divided by database length.
+        :return: average transaction length
+        """
+        totalLength = sum(self.lengthList)
+        averageLength = totalLength / len(self.database)
+        return averageLength
+
+    def getMaximumTransactionLength(self):
+        """
+        get the maximum transaction length
+        :return: maximum transaction length
+        """
+        maxLength = max(self.lengthList)
+        return maxLength
+
+    def getStandardDeviationTransactionLength(self):
+        """
+        get the standard deviation transaction length
+        :return: standard deviation transaction length
+        """
+        standardDeviation = statistics.pstdev(self.lengthList)
+        return standardDeviation
+
+    def getSortedListOfItemFrequencies(self):
+        """
+        get sorted list of item frequencies
+        :return: item frequencies
+        """
+        itemFrequencies = {}
+        for tid in self.database:
+            for item in self.database[tid]:
+                itemFrequencies[item] = itemFrequencies.get(item, 0)
+                itemFrequencies[item] += 1
+        itemFrequencies = {k: v for k, v in sorted(itemFrequencies.items(), key=lambda x:x[1], reverse=True)}
+        return itemFrequencies
+
+    def getTransanctionalLengthDistribution(self):
+        """
+        get transaction length
+        :return: transaction length
+        """
+        transactionLength = {}
+        for length in self.lengthList:
+            transactionLength[length] = transactionLength.get(length, 0)
+            transactionLength[length] += 1
+        transactionLength = {k: v for k, v in sorted(self.database.items(), key=lambda x:x[0])}
+        return transactionLength
+
+    def storeInFile(self, data, outputFile):
+        """
+        store data into outputFile
+        :param data: input data
+        :type data: dict
+        :param outputFile: output file name or path to store
+        :type outputFile: str
+        """
+        with open(outputFile, 'w') as f:
+            for key, value in data.items():
+                f.write(f'{key},{value}\n')
---
+++ PAMI/extras/euclidDistance.py
@@ -0,0 +1,72 @@
+import sys
+import re
+from math import sqrt
+
+class EuclidDistance:
+    """
+    This class create a neighbourhood file using euclid distance.
+    Attribute:
+    ----------
+        iFile : file
+            Input file name or path of the input file
+        oFile : file
+            Output file name or path pf the output file
+        threshold : int
+            The user can specify threshold.
+            This program find pairs of values whose Euclidean distance is less than or equal to threshold
+            and store the pairs.
+    Methods:
+        startMine()
+            find and store the pairs of values whose Euclidean distance is less than or equal to threshold.
+        getFileName()
+            This function returns output file name.
+    """
+
+    def __init__(self,iFile,oFile,threshold):
+        self.iFile = iFile
+        self.oFile = oFile
+        self.threshold = threshold
+
+    def startMine(self):
+        coordinates = []
+        result = {}
+        with open(self.iFile,"r") as f:
+            for line in f:
+                l = line.rstrip().split("\t")
+                l[0] = re.sub(r'[^0-9. ]', '', l[0])
+                coordinates.append(l[0].rstrip().split(' '))
+
+        for i in range(len(coordinates)):
+            for j in range(len(coordinates)):
+                if i != j:
+                    firstCoordinate = coordinates[i]
+                    secondCoordinate = coordinates[j]
+                    x1 = float(firstCoordinate[0])
+                    y1 = float(firstCoordinate[1])
+                    x2 = float(secondCoordinate[0])
+                    y2 = float(secondCoordinate[1])
+                    ansX = x2-x1
+                    ansY = y2-y1
+                    dist = abs(pow(ansX,2) - pow(ansY,2))
+                    norm = sqrt(dist)
+                    if norm <= float(self.threshold):
+                        result[tuple(firstCoordinate)] = result.get(tuple(firstCoordinate),[])
+                        result[tuple(firstCoordinate)].append(secondCoordinate)
+
+        with open(self.oFile,"w") as f:
+            for i in result:
+                string = i[0]+" "+i[1]+"\t"
+                f.write(string)
+                for j in result[i]:
+                    string = j[0] + " " + j[1] + "\t"
+                    f.write(string)
+                f.write("\n")
+
+
+    def getFileName(self):
+        return self.oFile
+
+if __name__ == "__main__":
+    euclid = EuclidDistance(sys.argv[1],sys.argv[2],sys.argv[3])
+    euclid.startMine()
+
