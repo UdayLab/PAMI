aac3f448cf665e1b77b21d874d8c13a1bc9c55db
Rage Uday Kiran <uday.rage@gmail.com>
2021-09-08 15:23:14 +0900

    setup

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         'psutil',
         'pandas',
         'matplotlib',
+        'resource',
     ],
     classifiers = [
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
