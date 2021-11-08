56a7da622ad373e2fa2128eb880191c9e4d6486e
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-11-08 13:14:30 +0530

    Update UPFPGrowth.py

--- PAMI/uncertainPeriodicFrequentPattern/basic/UPFPGrowth.py
+++ PAMI/uncertainPeriodicFrequentPattern/basic/UPFPGrowth.py
@@ -13,7 +13,7 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from abstract import *
+from PAMI.uncertainPeriodicFrequentPattern.basic.abstract import *
 
 minSup = float()
 maxPer = float()
