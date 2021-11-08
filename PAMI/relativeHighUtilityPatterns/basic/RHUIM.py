bce222c57d4bbcfef6434c62588f493c8edb4617
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-11-08 14:14:29 +0530

    Update RHUIM.py

--- PAMI/relativeHighUtilityPatterns/basic/RHUIM.py
+++ PAMI/relativeHighUtilityPatterns/basic/RHUIM.py
@@ -14,7 +14,7 @@
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from abstract import *
+from PAMI.relativeHighUtilityPatterns.basic.abstract import *
 
 
 class Transaction:
