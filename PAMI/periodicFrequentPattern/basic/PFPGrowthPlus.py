ec9ed6193b60213d8ae549c64c4bea0659d3fc17
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-11-08 14:09:55 +0530

    Update PFPGrowthPlus.py

--- PAMI/periodicFrequentPattern/basic/PFPGrowthPlus.py
+++ PAMI/periodicFrequentPattern/basic/PFPGrowthPlus.py
@@ -14,7 +14,7 @@
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from abstract import *
+from PAMI.periodicFrequentPattern.basic.abstract import *
 
 maxPer = float()
 minSup = float()
