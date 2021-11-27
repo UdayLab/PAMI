205e380ad85f1e63ec55c7bed85d5339cac8156a
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2021-11-27 09:17:10 +0530

    Update CPGrowthPlus.py

--- PAMI/correlatedPattern/basic/CPGrowthPlus.py
+++ PAMI/correlatedPattern/basic/CPGrowthPlus.py
@@ -677,22 +677,22 @@ class CPGrowthPlus(_ab._correlatedPatterns):
 
 
 if __name__ == "__main__":
-    ap = str()
+    _ap = str()
     if len(_ab._sys.argv) == 5 or len(_ab._sys.argv) == 6:
         if len(_ab._sys.argv) == 6:
-            ap = CPGrowthPlus(_ab._sys.argv[1], _ab._sys.argv[3], float(_ab._sys.argv[4]), _ab._sys.argv[5])
+            _ap = CPGrowthPlus(_ab._sys.argv[1], _ab._sys.argv[3], float(_ab._sys.argv[4]), _ab._sys.argv[5])
         if len(_ab._sys.argv) == 5:
-            ap = CPGrowthPlus(_ab._sys.argv[1], _ab._sys.argv[3], float(_ab._sys.argv[4]))
-        ap.startMine()
-        correlatedPatterns = ap.getPatterns()
-        print("Total number of Correlated-Frequent Patterns:", len(correlatedPatterns))
-        ap.savePatterns(_ab._sys.argv[2])
-        memUSS = ap.getMemoryUSS()
-        print("Total Memory in USS:", memUSS)
-        memRSS = ap.getMemoryRSS()
-        print("Total Memory in RSS", memRSS)
-        run = ap.getRuntime()
-        print("Total ExecutionTime in seconds:", run)
+            _ap = CPGrowthPlus(_ab._sys.argv[1], _ab._sys.argv[3], float(_ab._sys.argv[4]))
+        _ap.startMine()
+        _correlatedPatterns = _ap.getPatterns()
+        print("Total number of Correlated-Frequent Patterns:", len(_correlatedPatterns))
+        _ap.savePatterns(_ab._sys.argv[2])
+        _memUSS = _ap.getMemoryUSS()
+        print("Total Memory in USS:", _memUSS)
+        _memRSS = _ap.getMemoryRSS()
+        print("Total Memory in RSS", _memRSS)
+        _run = _ap.getRuntime()
+        print("Total ExecutionTime in seconds:", _run)
     else:
         '''l = [0.0007, 0.0009, 0.001, 0.002, 0.003, 0.01]
         for i in l:
