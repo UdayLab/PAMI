f606f303ec00d5461e7669184f05bfa76d2dde6b
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2022-05-18 13:10:24 +0900

    Update parallelECLAT.py

--- PAMI/frequentPattern/pyspark/parallelECLAT.py
+++ PAMI/frequentPattern/pyspark/parallelECLAT.py
@@ -47,14 +47,50 @@ class parallelECLAT(_ab._frequentPatterns):
 
         Executing the code on terminal:
         -------------------------------
-
             Format:
             ------
-                python3 parallelApriori.py <inputFile> <outputFile> <minSup> <numWorkers>
-
+            
+                python3 parallelECLAT.py <inputFile> <outputFile> <minSup> <numWorkers>
+            
             Examples:
             ---------
-                python3 Apriori.py sampleDB.txt patterns.txt 1000 10  (minSup will be considered in support count or frequency)
+            
+                python3 parallelECLAT.py sampleDB.txt patterns.txt 10.0 3   (minSup will be considered in times of minSup and count of database transactions)
+            
+                python3 paralleECLAT.py sampleDB.txt patterns.txt 10 3    (minSup will be considered in support count or frequency)
+        
+        Sample run of the importing code:
+        ---------------------------------
+            
+            import PAMI.frequentPattern.pyspark.parallelECLAT as alg
+            
+            obj = alg.parallelECLAT(iFile, minSup)
+            
+            obj.startMine()
+            
+            frequentPatterns = obj.getPatterns()
+            
+            print("Total number of Frequent Patterns:", len(frequentPatterns))
+            
+            obj.savePatterns(oFile)
+            
+            Df = obj.getPatternInDataFrame()
+            
+            memUSS = obj.getMemoryUSS()
+            
+            print("Total Memory in USS:", memUSS)
+            
+            memRSS = obj.getMemoryRSS()
+            
+            print("Total Memory in RSS", memRSS)
+            
+            run = obj.getRuntime()
+            
+            print("Total ExecutionTime in seconds:", run)
+        
+        Credits:
+        --------
+            The complete program was written by Yudai Masu  under the supervision of Professor Rage Uday Kiran.
         """
 
     _minSup = float()
