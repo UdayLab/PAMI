a77b0f3b17d73142550023f386f57ca7673ff643
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2022-06-20 12:16:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     install_requires=[            # All necessary packages utilized by our PAMI software
         'psutil',
         'pandas',
+	'plotly',
         'matplotlib',
         'resource',
         'validators',
