87a4262ff201e225c06fa80ab8562bd5c25482f1
Likhitha-palla <70084044+Likhitha-palla@users.noreply.github.com>
2022-06-24 10:49:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'pami',
-    version = '2022.06.4',
+    version = '2022.06.20.5',
     author = 'Rage Uday Kiran',
     author_email = 'uday.rage@gmail.com',
     description = 'This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
