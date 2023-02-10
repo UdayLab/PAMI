98f9449f210719e8611a955d3554224fa34c98d3
Likhitha <70084044+Likhitha-palla@users.noreply.github.com>
2023-02-11 08:07:30 +0900

    Update SPADE.py

--- PAMI/sequentialPatternMining/basic/SPADE.py
+++ PAMI/sequentialPatternMining/basic/SPADE.py
@@ -14,7 +14,7 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import abstract as _ab
+from PAMI.sequentialPatternMining.basic import abstract as _ab
 
 _ab._sys.setrecursionlimit(10000)
 
