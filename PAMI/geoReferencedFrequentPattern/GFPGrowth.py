81570dcc6c2cc9545999430a23c151b9581ebb4e
Likhitha <70084044+Likhitha-palla@users.noreply.github.com>
2023-05-30 12:12:59 +0900

    Update GFPGrowth.py

--- PAMI/geoReferencedFrequentPattern/GFPGrowth.py
+++ PAMI/geoReferencedFrequentPattern/GFPGrowth.py
@@ -13,7 +13,7 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from geoReferencedFrequentPatterns import abstract as _ab
+from PAMI.geoReferencedFrequentPatterns import abstract as _ab
 
 _minSup = str()
 _neighbourList = {}
