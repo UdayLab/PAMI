ca2b115783bca05349ae0bd13ad4d4082452ddaa
s1280130 <s1280130@u-aizu.ac.jp>
2023-07-24 18:32:54 +0900

    update description

--- PAMI/georeferencedFrequentSequencePattern/basic/GFSPminer.py
+++ PAMI/georeferencedFrequentSequencePattern/basic/GFSPminer.py
@@ -93,6 +93,28 @@ class GFSPminer(_ab._GeorefarencedFequentialPatterns):
                 find x-1 lenghth frequent pattern by convine two x len patterns
             makexLenDatabase(rowLen, bs, latestWord)
                 To make "rowLen" length frequent patterns from pattern which latest word is in same seq  by joining "rowLen"-1 length pattrens by depth-first search technique  and update xlenDatabase to seqential database
+            makeSame(rowLen, bs, latestWord, latestWord2):
+                    to check the pattern is frequent or not (ex a-bc from a-c and a-b )
+            makeFaster(rowLen, bs, latestWord, latestWord2):
+                    to check the pattern is frequent or not (ex a-b-c from a-c a-b)
+            makeLater(rowLen, bs, latestWord, latestWord2):
+                    to check the pattern is frequent or not (ex ac-b from a-b ac)
+            makeSame2(rowLen, bs, latestWord, latestWord2):
+                    to check the pattern is frequent or not (ex a-bc from a-c ab)
+            makeSame3(rowLen, bs, latestWord, latestWord2):
+                    to check the pattern is frequent or not (ex abc from ac ab)
+            makexLenDatabaseSame(rowLen, bs, latestWord):
+                    To make 3 or more length frequent patterns from pattern which latest word is in different seq  by depth-first search technique  and update xlenDatabase to seqential database
+            makeNextRow(bs, latestWord, latestWord2):
+                       To make pattern row when two patterns have latest word in different sequence
+            makeNextRowSame(bs, latestWord, latestWord2):
+                        To make pattern row when one pattern have latestword1 in different sequence and other(latestword2) in same
+            makeNextRowSame2(bs, latestWord, latestWord2):
+                        To make pattern row when two patterns have latest word in same sequence
+            makeNextRowSame3(bs, latestWord, latestWord2):
+                  To make pattern row when two patterns have latest word in different sequence and both latest word is in same sequence
+
+
 
 
 
