495031235bfc8b303445a880af0592c85e60e2ef
udaykiranrage <uday.rage@gmail.com>
2023-07-28 22:11:11 +0900

    parallelPFP-growth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pami',
-    version='2023.07.28.3',
+    version='2023.07.28.4',
     author='Rage Uday Kiran',
     author_email='uday.rage@gmail.com',
     description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
