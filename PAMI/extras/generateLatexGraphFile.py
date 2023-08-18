83c52c16607a01bf54ca12fec5add97211d9c486
PallaMadhavi <pallamadhavi69@gmail.com>
2023-08-18 12:14:07 +0530

    correcting typos

--- PAMI/extras/generateLatexGraphFile.py
+++ PAMI/extras/generateLatexGraphFile.py
@@ -1,4 +1,51 @@
+# generateLatexGraphFile is used to convert the given data into LatexGraphFile.
+#
+# **Importing this algorithm into a python program**
+# --------------------------------------------------------
+#
+#     from PAMI.extras.syntheticDatabaseGenerator import generateLatexGraphFile as fuz
+#
+#     obj = fuz.generateLatexGraphFile(idf)
+#
+#     obj.save()
+#
+#
+
+__copyright__ = """
+ Copyright (C)  2021 Rage Uday Kiran
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import pandas as pd
+
+class generateLatexGraphFile():
+    """
+        :Description:
+                generateLatexGraphFile is used to convert the given data into LatexGraphFile.
+
+        **Importing this algorithm into a python program**
+        --------------------------------------------------------
+        .. code-block:: python
+
+        from PAMI.extras.syntheticDatabaseGenerator import generateLatexGraphFile as fuz
+
+        obj = fuz.generateLatexGraphFile(idf)
+
+        obj.save()
+
+    """
+
 
 
 def generateLatexCode(result: pd.DataFrame) -> None:
