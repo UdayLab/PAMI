5e7077bef7d8775282cd6357828a49bdf7da4967
Likhitha <likhithapalla7@gmail.com>
2023-10-07 20:51:09 +0530

    correcting typos

--- PAMI/coveragePattern/basic/CPPG.py
+++ PAMI/coveragePattern/basic/CPPG.py
@@ -120,31 +120,31 @@ class CPPG(_ab._coveragePatterns):
 
     .. code-block:: python
 
-                from PAMI.coveragePattern.basic import CPPG as alg
+            from PAMI.coveragePattern.basic import CPPG as alg
 
-                obj = alg.CPPG(iFile, minRF, minCS, maxOR)
+            obj = alg.CPPG(iFile, minRF, minCS, maxOR)
 
-                obj.startMine()
+            obj.startMine()
 
-                coveragePattern = obj.getPatterns()
+            coveragePattern = obj.getPatterns()
 
-                print("Total number of coverage Patterns:", len(coveragePattern))
+            print("Total number of coverage Patterns:", len(coveragePattern))
 
-                obj.save(oFile)
+            obj.save(oFile)
 
-                Df = obj.getPatternsAsDataFrame()
+            Df = obj.getPatternsAsDataFrame()
 
-                memUSS = obj.getMemoryUSS()
+            memUSS = obj.getMemoryUSS()
 
-                print("Total Memory in USS:", memUSS)
+            print("Total Memory in USS:", memUSS)
 
-                memRSS = obj.getMemoryRSS()
+            memRSS = obj.getMemoryRSS()
 
-                print("Total Memory in RSS", memRSS)
+            print("Total Memory in RSS", memRSS)
 
-                run = obj.getRuntime()
+            run = obj.getRuntime()
 
-                print("Total ExecutionTime in seconds:", run)
+            print("Total ExecutionTime in seconds:", run)
 
 
     **Credits:**
