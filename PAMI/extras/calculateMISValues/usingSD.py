0981859c3ba09301aceb1a2e1f08b08bbaa94eec
Likhitha <70084044+Likhitha-palla@users.noreply.github.com>
2023-10-19 09:02:44 +0530

    Update usingSD.py

--- PAMI/extras/calculateMISValues/usingSD.py
+++ PAMI/extras/calculateMISValues/usingSD.py
@@ -35,7 +35,7 @@ import validators as _validators
 import statistics as _statistics
 from urllib.request import urlopen as _urlopen
 
-class usingBeta():
+class usingSD():
     """
 
             :Description: This code is used to calculate multiple minimum support of items in the the given database. Output can be stored in file or as as dataframe.
@@ -156,7 +156,7 @@ class usingBeta():
         self._oFile = outFile
         writer = open(self._oFile, 'w+')
         for x, y in self._finalPatterns.items():
-            patternsAndSupport = x + ":" + str(y)
+            patternsAndSupport = x + "\t" + str(y)
             writer.write("%s \n" % patternsAndSupport)
 
 if __name__ == '__main__':
