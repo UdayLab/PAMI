256d640ab55b36005310a0dc5dd5dc063623553d
vanithakattumuri <vanithakattumuri32@gmail.com>
2023-10-28 16:13:16 +0900

    updated

--- PAMI/extras/graph/plotLineGraphFromDictionary.py
+++ PAMI/extras/graph/plotLineGraphFromDictionary.py
@@ -72,8 +72,8 @@ class plotLineGraphFromDictionary:
         """
         end = int(len(data) * end / 100)
         start = int(len(data) * start / 100)
-        x = tuple(data.keys())[start:end]
-        y = tuple(data.values())[start:end]
+        x = list(range(len(data)))
+        y = list(tuple(data.values())[start:end])
         fig, ax = plt.subplots()
         ax.plot(x, y)
         ax.set_title(title)
