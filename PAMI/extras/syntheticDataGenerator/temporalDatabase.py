bf00c21e6f67d39153fb834b859eea97965c1c5a
Avvari1830S <76546836+Avvari1830S@users.noreply.github.com>
2023-11-04 01:09:32 +0900

    Update temporalDatabase.py

--- PAMI/extras/syntheticDataGenerator/temporalDatabase.py
+++ PAMI/extras/syntheticDataGenerator/temporalDatabase.py
@@ -8,18 +8,19 @@ class TemporalDataGenerator:
         self.avg_transaction_length = avg_transaction_length
         self.transactions = []
 
-    def generate_data(self):
+    def generate(self):
         for transaction_id in range(self.transaction_size):
             transaction_length = np.random.randint(1, self.avg_transaction_length * 2)
             transaction = np.random.choice(range(1, self.num_of_items + 1), size=transaction_length)
             self.transactions.append(transaction)
 
-    def save_to_csv(self, output_file):
-        data = {'Transaction ID': range(1, len(self.transactions) + 1), 'Items': self.transactions}
-        df = pd.DataFrame(data)
-        df.to_csv(output_file, sep='\t', index=False)
+    def save(self, output_file):
+        with open(output_file, 'w') as file:
+            for idx, transaction in enumerate(self.transactions, start=1):
+                transaction_str = '\t'.join(map(str, transaction))
+                file.write(f'{idx}\t{transaction_str}\n')
 
 if __name__ == "__main__":
     data_generator = TemporalDataGenerator(10, 10, 5)
-    data_generator.generate_data()
-    data_generator.save_to_csv("temporal_test.csv")
+    data_generator.generate()
+    data_generator.save("temporal_test.csv")
