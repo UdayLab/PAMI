262af54fb676838065ddc0621f325f7433ec3f5f
Avvari1830S <76546836+Avvari1830S@users.noreply.github.com>
2024-03-11 13:04:00 +0900

    Update temporalDatabaseGen.py

--- PAMI/extras/syntheticDataGenerator/temporalDatabaseGen.py
+++ PAMI/extras/syntheticDataGenerator/temporalDatabaseGen.py
@@ -1,68 +1,60 @@
-import random as _rd
+import random
 
 
-class CreateSyntheticTemporal:
+class TransactionalDatabase:
     """
-    This class creates a synthetic temporal database.
+    A class to generate a transactional database with random transactions.
 
     Attributes:
-        total_transactions (int): Number of transactions.
-        num_of_items (int): Number of items.
-        avg_transaction_length (int): The length of average transaction.
+        databaseSize (int): The number of transactions in the database.
+        numOfItems (int): The maximum number of items in each transaction.
+        avgTransactionLength (int): The average length of transactions.
+        transactions (list): List to store generated transactions.
 
     Methods:
-        create_temporal_database(output_file)
-            Create temporal database and store it in the specified output file.
+        __init__(self, database_size, num_of_items, avg_transaction_length): Constructor to initialize the database parameters.
+        generate(): Method to generate random transactions based on the specified parameters.
+        generateRandomNumbers(n, target_sum): Method to generate a list of random numbers with a specified target sum.
+        save(output_file, sep): Method to save the generated transactions to a CSV file.
 
-    Credits:
-        The complete program was written by A.Hemanth sree sai under the supervision of Professor Rage Uday Kiran.
+    Usage:
+        obj = TransactionalDatabase(database_size, num_of_items, avg_transaction_length)
+        obj.generate()
+        obj.save("transactional_output.csv")
     """
 
-    def __init__(self, total_transactions: int, num_of_items: int, avg_transaction_length: int) -> None:
+    def __init__(self, database_size: int, num_of_items: int, avg_transaction_length: int) -> None:
         """
         Constructor to initialize the database parameters.
 
         Parameters:
-            total_transactions (int): Number of transactions.
-            num_of_items (int): Number of items.
-            avg_transaction_length (int): The length of average transaction.
+            database_size (int): The number of transactions in the database.
+            num_of_items (int): The maximum number of items in each transaction.
+            avg_transaction_length (int): The average length of transactions.
         """
-        self.total_transactions = total_transactions
-        self.num_of_items = num_of_items
-        self.avg_transaction_length = avg_transaction_length
+        self.databaseSize = database_size
+        self.numOfItems = num_of_items
+        self.avgTransactionLength = avg_transaction_length
+        self.transactions = list()
 
-    def create_temporal_database(self, output_file: str) -> None:
+    def generate(self) -> None:
         """
-        Create temporal database and store it in the specified output file.
-
-        Parameters:
-            output_file (str): File name or path to store the database.
+        Method to generate random transactions based on the specified parameters.
+        If avg_transaction_length exceeds num_of_items, an error message is printed.
         """
-        if self.avg_transaction_length > self.num_of_items:
+        if self.avgTransactionLength > self.numOfItems:
             print("Error: avg_transaction_length cannot exceed num_of_items.")
             return
 
-        count = 1
-        with open(output_file, 'w') as writer:
-            for _ in range(self.total_transactions):
-                length = _rd.randint(1, self.avg_transaction_length + 20)
-                st = str(count) + '\t'
-                random_numbers = self.generate_random_numbers(13, 2000)
+        for _ in range(self.databaseSize):
+            length = random.randint(1, self.avgTransactionLength * 2)
+            transaction = [random.randint(1, self.numOfItems)
+                           for _ in range(length)]
+            self.transactions.append(transaction)
 
-                # Checking if avgTransactionLength exceeds numOfItems
-                if self.avg_transaction_length > self.num_of_items:
-                    print("Error: avg_transaction_length cannot exceed num_of_items.")
-                    return
-
-                for _ in range(length):
-                    item = _rd.randint(1, self.num_of_items)
-                    st = st + str(item) + '\t'
-                writer.write("%s \n" % st)
-                count += 1
-
-    def generate_random_numbers(self, n: int, target_sum: int) -> list[float]:
+    def generateRandomNumbers(self, n: int, target_sum: int) -> list[float]:
         """
-        Generate a list of random numbers with a specified target sum.
+        Method to generate a list of random numbers with a specified target sum.
 
         Parameters:
             n (int): Number of random numbers to generate.
@@ -71,15 +63,33 @@ class CreateSyntheticTemporal:
         Returns:
             list: List of generated random numbers normalized and multiplied by the target sum.
         """
-        rand_numbers = [_rd.uniform(0, 1) for _ in range(n)]
+        rand_numbers = [random.uniform(0, 1) for _ in range(n)]
         rand_sum = sum(rand_numbers)
         normalized_numbers = [num / rand_sum for num in rand_numbers]
         result = [round(num * target_sum) for num in normalized_numbers]
         return result
 
+    def save(self, output_file: str, sep="\t") -> None:
+        """
+        Method to save the generated transactions to a CSV file.
+
+        Parameters:
+            output_file (str): File path for the CSV file.
+            sep (str): Separator for the CSV file (default is '\t').
+        """
+        with open(output_file, 'w') as f:
+            for transaction in self.transactions:
+                # Adding the generated random numbers to each transaction
+                random_numbers = self.generateRandomNumbers(13, 2000)
+                transaction += random_numbers
+
+                f.write(f"{sep.join(map(str, transaction))}\n")
+
 
 if __name__ == "__main__":
-    ap = CreateSyntheticTemporal(100000, 870, 10)
-    ap.create_temporal_database("temporal_T10.csv")
-else:
-    print("Error! The number of input parameters does not match the total number of parameters provided")
+    obj = TransactionalDatabase(10, 10, 5)
+    obj.generate()
+
+    # Saving transactions to a CSV file with generated random numbers
+    obj.save("transactional_output.csv")
+    
