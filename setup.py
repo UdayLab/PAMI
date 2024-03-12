880cccf7d641305082c6fe42f7e85d43f4a7c065
vanithakattumuri <vanithakattumuri32@gmail.com>
2024-03-12 14:01:12 +0900

    main pami

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pami',
-    version='2024.3.12.2',
+    version='2024.3.12.1',
     author='Rage Uday Kiran',
     author_email='uday.rage@gmail.com',
     description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
@@ -43,3 +43,4 @@ setuptools.setup(
     ],
     python_requires='>=3.5',
 )
+
