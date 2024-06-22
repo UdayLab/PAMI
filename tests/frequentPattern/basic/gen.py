9fbe09c3a9d626071243c525ad5bf8561cb75205
Likhitha <likhithapalla7@gmail.com>
2024-06-22 13:47:29 +0900

    Automated Testing

---
+++ tests/frequentPattern/basic/gen.py
@@ -0,0 +1,20 @@
+import random
+import warnings
+warnings.filterwarnings("ignore")
+
+def generate_transactional_dataset(num_transactions, items, max_items_per_transaction):
+    dataset = []
+    for _ in range(num_transactions):
+        num_items = random.randint(1, max_items_per_transaction)
+        transaction = random.sample(items, num_items)
+        dataset.append(transaction)
+    return dataset
+
+
+# num_distinct_items=20
+# num_transactions = 1000
+# max_items_per_transaction = 20
+# items=["item-{}".format(i) for i in range(1,num_distinct_items+1)]
+
+# dataset = generate_transactional_dataset(num_transactions, items, max_items_per_transaction)
+# print(dataset)
