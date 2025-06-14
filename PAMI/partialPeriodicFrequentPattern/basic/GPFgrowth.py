0b5fcbca7fbf2a050266f7e705071c35902c0b6a
lasya <lasya.palla9705@gmail.com>
2025-06-14 19:17:06 +0900

    reverted GPFGrowth

--- PAMI/partialPeriodicFrequentPattern/basic/GPFgrowth.py
+++ PAMI/partialPeriodicFrequentPattern/basic/GPFgrowth.py
@@ -406,19 +406,19 @@ class GPFgrowth(partialPeriodicPatterns, ABC):
                 else:
                     transactions[tuple(transaction)] = locs
 
-                for item1 in transaction:
-                    if item1 in itemLocs:
-                        itemLocs[item1] += locs
+                for item in transaction:
+                    if item in itemLocs:
+                        itemLocs[item] += locs
                     else:
-                        itemLocs[item1] = list(locs)
+                        itemLocs[item] = list(locs)
 
             # Precompute getMaxPer results for itemLocs
             # maxPerResults = {item: self._getMaxPer(itemLocs[item], maxTS) for item in itemLocs if len(itemLocs[item]) >= minSup}
             maxPerResults = {item: self._ratioCalc(itemLocs[item]) for item in itemLocs if len(itemLocs[item]) >= self._partialPeriodicPatterns__minSup}
 
-            for item3 in maxPerResults:
-                if maxPerResults[item3] >= self._partialPeriodicPatterns__minPR:
-                    self._partialPeriodicPatterns__finalPatterns[tuple(newRoot.item3 + [item3])] = [len(itemLocs[item3]), maxPerResults[item3]]
+            for item in maxPerResults:
+                if maxPerResults[item] >= self._partialPeriodicPatterns__minPR:
+                    self._partialPeriodicPatterns__finalPatterns[tuple(newRoot.item + [item])] = [len(itemLocs[item]), maxPerResults[item]]
 
             # Filter itemLocs based on minSup and maxPer
             itemLocs = {k: len(v) for k, v in itemLocs.items() if len(v) >= self._partialPeriodicPatterns__minSup}
@@ -433,12 +433,12 @@ class GPFgrowth(partialPeriodicPatterns, ABC):
                 if len(transaction) < 1:
                     continue
                 currNode = newRoot
-                for item2 in transaction:
-                    currNode = currNode.addChild(item2, locs)
-                    if item2 in newItemNodes:
-                        newItemNodes[item2].add(currNode)
+                for item in transaction:
+                    currNode = currNode.addChild(item, locs)
+                    if item in newItemNodes:
+                        newItemNodes[item].add(currNode)
                     else:
-                        newItemNodes[item2] = set([currNode])
+                        newItemNodes[item] = set([currNode])
 
             self._recursive(newRoot, newItemNodes)
 
