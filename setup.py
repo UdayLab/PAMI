2e868570b1349645db234cd62a1f0c8cc1db9a12
uday <uday.rage@gmail.com>
2025-07-03 10:25:47 +0900

    updated setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pami',
-    version='2025.06.26.1',
+    version='2025.07.3.1',
     author='Rage Uday Kiran',
     author_email='uday.rage@gmail.com',
     description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
