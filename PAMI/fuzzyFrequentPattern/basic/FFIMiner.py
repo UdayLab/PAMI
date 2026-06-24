43056957c2655a474f490c679a8ed3763f80d256
MithunThangaraj <133865250+MithunThangaraj@users.noreply.github.com>
2026-06-24 15:39:07 +0900

    added k-cardinality parameter to FFIMiner

--- PAMI/fuzzyFrequentPattern/basic/FFIMiner.py
+++ PAMI/fuzzyFrequentPattern/basic/FFIMiner.py
@@ -164,7 +164,7 @@ class FFIMiner(_ab._fuzzyFrequentPatterns):
     _memoryRSS = float()
     _sep = "\t"
 
-    def __init__(self, iFile: str, minSup: float, sep: str="\t") -> None:
+    def __init__(self, iFile: str, minSup: float, sep: str = "\t", k: int = 1) -> None:
         super().__init__(iFile, minSup, sep)
         self._Database = None
         self._startTime = 0
@@ -173,6 +173,7 @@ class FFIMiner(_ab._fuzzyFrequentPatterns):
         self._minSup = minSup
         self._iFile = iFile
         self._sep = sep
+        self._k = k
         self._finalPatterns = {}
         self._memoryUSS = 0
         self._memoryRSS = 0
@@ -219,7 +220,7 @@ class FFIMiner(_ab._fuzzyFrequentPatterns):
                 except IOError:
                     print("File Not Found")
                     quit()
-    
+
     def startMine(self):
         self.mine()
 
@@ -243,7 +244,7 @@ class FFIMiner(_ab._fuzzyFrequentPatterns):
             else:
                 value = int(value)
         return value
-    
+
     def dfs(self, cands):
         """
         Perform depth-first search (DFS) to find frequent patterns in a database.
@@ -295,6 +296,20 @@ class FFIMiner(_ab._fuzzyFrequentPatterns):
                 if item not in items:
                     items[item] = {}
                 items[item][lineNo] = fuzzyValue
+
+        if self._k >= 1:
+            #default k = 1 (max cardinality)
+            labelsByBaseItem = {}
+            for label in items:
+                token = label[0]
+                baseItem = token.rsplit('.', 1)[0] if '.' in token else token
+                labelsByBaseItem.setdefault(baseItem,[]).append(label)
+            for baseItem,labels in labelsByBaseItem.items():
+                if len(labels) <= self._k:
+                    continue
+                ranked = sorted(labels, key=lambda lbl: sum(items[lbl].values()), reverse=True)
+                for lbl in ranked[self._k:]:
+                    del items[lbl]
 
         self._minSup = self._convert(self._minSup)
         self._Database = items.copy()
