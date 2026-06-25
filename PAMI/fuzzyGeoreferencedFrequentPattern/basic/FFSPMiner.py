65482eb8546ffa5e2d726fb1d21d70d52e6bb4f1
MithunThangaraj <133865250+MithunThangaraj@users.noreply.github.com>
2026-06-25 13:19:34 +0900

    added k-cardinality parameter to FFSPMiner

--- PAMI/fuzzyGeoreferencedFrequentPattern/basic/FFSPMiner.py
+++ PAMI/fuzzyGeoreferencedFrequentPattern/basic/FFSPMiner.py
@@ -162,6 +162,8 @@ class FFSPMiner(_ab._fuzzySpatialFrequentPatterns):
                    Name of the input file to mine complete set of frequent patterns
     :param  sep: str :
                    This variable is used to distinguish items from one another in a transaction. The default seperator is tab space. However, the users can override their default separator.
+    :param  k: int :
+                   Number of top fuzzy terms to keep per item, ranked by total fuzzy value. k=1 (default) keeps only the best term per item, k=2 the top two
 
 
     :Attributes:
@@ -278,13 +280,14 @@ class FFSPMiner(_ab._fuzzySpatialFrequentPatterns):
     _nFile = " "
     _sep = "\t"
 
-    def __init__(self, iFile: str, nFile: str, minSup: float, sep: str="\t") -> None:
+    def __init__(self, iFile: str, nFile: str, minSup: float, sep: str = "\t", k: int = 1) -> None:
         super().__init__(iFile, nFile, minSup, sep)
         self.oFile = None
         self._mapItemNeighbours = {}
         self._startTime = 0
         self._endTime = 0
         self._mapItemSum = {}
+        self._k = k
         self._joinsCnt = 0
         self._BufferSize = 200
         self._itemSetBuffer = []
@@ -446,6 +449,20 @@ class FFSPMiner(_ab._fuzzySpatialFrequentPatterns):
                     self._mapItemSum[item] += quantities[i]
                 else:
                     self._mapItemSum[item] = quantities[i]
+
+        if self._k >= 1:
+            #default k = 1 (max cardinality)
+            labelsByBaseItem = {}
+            for label in self._mapItemSum:
+                baseItem = label.rsplit('.', 1)[0] if '.' in label else label
+                labelsByBaseItem.setdefault(baseItem, []).append(label)
+            for baseItem, labels in labelsByBaseItem.items():
+                if len(labels) <= self._k:
+                    continue
+                ranked = sorted(labels, key=lambda lbl: self._mapItemSum[lbl], reverse=True)
+                for lbl in ranked[self._k:]:
+                    del self._mapItemSum[lbl]
+
         listOfFFList = []
         mapItemsToFFLIST = {}
         #self._minSup = self._convert(self._minSup)
@@ -466,7 +483,7 @@ class FFSPMiner(_ab._fuzzySpatialFrequentPatterns):
                 pair.item = items[i]
                 pair.quantity = quantities[i]
                 item = pair.item
-                if self._mapItemSum[item] >= self._minSup:
+                if self._mapItemSum.get(item, 0) >= self._minSup:
                     if pair.quantity > 0:
                         revisedTransaction.append(pair)
             revisedTransaction.sort(key=_ab._functools.cmp_to_key(self._compareItems))
